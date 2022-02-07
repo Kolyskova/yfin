f8f2a33bfc5fb75e3eeb9a755470f2ab5f522299
Kristina S <56933527+Kolyskova@users.noreply.github.com>
2022-02-07 15:39:21 +0400

    Add files via upload

---
+++ interpolation.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+
+# добавление пропущенных дат
+def add_nan():
+    df = pd.read_csv('file1.csv')
+    idx = pd.date_range(df['Date'].min(), df['Date'].max())
+    df.index = pd.DatetimeIndex(df['Date'])
+    df = df.reindex(idx, fill_value='NaN')
+    del df['Date']
+    df.to_csv('file2.csv')
+
+
+# линейная интреполяция
+def linear():
+    df = pd.read_csv('file2.csv')
+    columns = list(df)
+    for i in columns:
+        df[i].interpolate(method='linear', inplace=True)
+    print(df)
+
+
+# квадратичная интерполяция
+def quadratic():
+    df = pd.read_csv('file2.csv')
+    columns = list(df)
+    for i in columns:
+        df[i].interpolate(method='quadratic', inplace=True)
+    print(df)
+
+
+# заполняет пропуски ближайшими значениями
+def nearest():
+    df = pd.read_csv('file2.csv')
+    columns = list(df)
+    for i in columns:
+        df[i].interpolate(method='nearest', inplace=True)
+    print(df)
+
+
+nearest()
+linear()
+quadratic()
